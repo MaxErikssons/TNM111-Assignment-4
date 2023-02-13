e17eb8deb0ca994b957ff93841389a624131343b
Max Eriksson <83306807+MaxErikssons@users.noreply.github.com>
2023-02-13 17:57:45 +0100

    Initial commit
    Functions to render a node-link diagram are provided, allowing the user to select any number of episodes and have a different diagram rendered for each selection. If no epsiodes are chosen, the "full-interactions" database is used to generate a diagram. However, there are some issues with labeling that need to be addressed.

---
+++ js/script.js
@@ -0,0 +1,168 @@
+let data;
+
+function fetchData() {
+  // Get all the checkbox elements with class "episode"
+  const checkboxes = document.querySelectorAll('.episode');
+
+  // Set default episode to null
+  let selectedEpisodes = [];
+
+  // Function to fetch and render data for a given episode
+  const fetchAndRenderData = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        data = json;
+
+        render();
+      });
+  };
+
+  // Loop through each checkbox
+  checkboxes.forEach((checkbox) =>
+    // Add change event listener to each checkbox
+    checkbox.addEventListener('change', (e) => {
+      // If a checkbox is checked, set episode to the value of the checkbox
+      if (e.target.checked) selectedEpisodes.push(e.target.value);
+      // If the checkbox is unchecked, set episode to null
+      else
+        selectedEpisodes = selectedEpisodes.filter(
+          (ep) => ep !== e.target.value
+        );
+
+      // Fetch and render data for all selected episodes
+      if (selectedEpisodes.length > 0) {
+        console.log(selectedEpisodes);
+        d3.selectAll('svg').remove();
+        selectedEpisodes.forEach((ep) => {
+          fetchAndRenderData(
+            `db/starwars-episode-${ep}-interactions-allCharacters.json`
+          );
+        });
+      } else {
+        // If no episode is selected, fetch data for full interactions
+        d3.selectAll('svg').remove();
+        fetchAndRenderData('db/starwars-full-interactions-allCharacters.json');
+      }
+    })
+  );
+
+  // When staring the program we get here, fetch data for full interactions
+  if (selectedEpisodes.length === 0) {
+    d3.selectAll('svg').remove();
+    fetchAndRenderData('db/starwars-full-interactions-allCharacters.json');
+  }
+}
+
+function render() {
+  console.log(data);
+
+  // Get the nodes and links from the data
+  const nodes = data.nodes;
+  const links = data.links;
+
+  // Set the dimensions of the diagram
+  const width = 600;
+  const height = 600;
+
+  // Create a force simulation to determine the position of the nodes
+  const simulation = d3
+    .forceSimulation()
+    .nodes(nodes)
+    .force('charge', d3.forceManyBody().strength(-100))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('link', d3.forceLink(links).distance(20));
+
+  // Create a svg element to hold the diagram
+  const svg = d3
+    .select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .style('display', 'inline-block')
+    .style('margin-right', '10px');
+
+  // Add the links to the svg element
+  const link = svg
+    .selectAll('.link')
+    .data(links)
+    .enter()
+    .append('line')
+    .attr('class', 'link')
+    .style('stroke', '#ededed')
+    .style('stroke-width', '1px');
+
+  // Add the nodes to the svg element
+  const node = svg
+    .selectAll('.node')
+    .data(nodes)
+    .enter()
+    .append('circle')
+    .attr('class', 'node')
+    .attr('r', 5)
+    .style('fill', (d) => d.colour)
+    .call(
+      d3
+        .drag()
+        .on('start', (event, d) => {
+          if (!event.active) {
+            simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+          }
+        })
+        .on('drag', (event, d) => {
+          d.fx = event.x;
+          d.fy = event.y;
+        })
+        .on('end', (event, d) => {
+          if (!event.active) {
+            simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+          }
+        })
+    );
+
+  //The labeling is problematic...
+  const label = svg
+    .selectAll('.label')
+    .data(nodes)
+    .enter()
+    .append('text')
+    .attr('class', 'label')
+    .text((d) => d.name)
+    .attr('x', (d) => d.x)
+    .attr('y', (d) => d.y)
+    .style('font-size', '10px');
+
+  // Start the simulation
+  simulation.nodes(nodes).on('tick', ticked);
+  simulation.force('link').links(links);
+
+  function ticked() {
+    link
+      .attr('x1', function (d) {
+        return d.source.x;
+      })
+      .attr('y1', function (d) {
+        return d.source.y;
+      })
+      .attr('x2', function (d) {
+        return d.target.x;
+      })
+      .attr('y2', function (d) {
+        return d.target.y;
+      });
+
+    node
+      .attr('cx', function (d) {
+        return d.x;
+      })
+      .attr('cy', function (d) {
+        return d.y;
+      });
+  }
+}
+
+fetchData();
